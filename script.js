218f2efadb6c2a1b0b3ed489ed33444589f7fa5d
l1ghtsh0w <48393303+l1ghtsh0w@users.noreply.github.com>
2019-03-09 14:55:03 -0600

    Initial
    Initial

---
+++ script.js
@@ -0,0 +1,18 @@
+function changeContent(file)
+{
+  file = "pages/" + file + ".html";
+  var rawFile = new XMLHttpRequest();
+  rawFile.open("GET", file, false);
+  rawFile.onreadystatechange = function ()
+  {
+    if (rawFile.readyState === 4)
+    {
+      if (rawFile.status === 200 || rawFile.status == 0)
+      {
+        var allText = rawFile.responseText;
+        document.getElementById("content").innerHTML = allText;
+      }
+    }
+  }
+  rawFile.send(null);
+}
