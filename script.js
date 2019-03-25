aa18cae11a0bf84f60b1f04eb7f37868a106e15c
l1ghtsh0w <48393303+l1ghtsh0w@users.noreply.github.com>
2019-03-25 10:25:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -60,12 +60,12 @@ var quotes = [
   ["I can imagine no more rewarding a career. And any man who may be asked in this century what he did to make his life worthwhile, I think can respond with a good deal of pride and satisfaction: 'I served in the United States Navy.'", "President John F. Kennedy"]
 ]
 
-const vsn = "1";
+const vsn = 1;
 
 function loaded()
 {
-  document.getElementById("version").innerHtml = "v." + vsn;
-  writeQuoutes();
+  document.getElementById("version").innerText = "v." + str(vsn);
+  writeQuotes();
 }
 
 function writeQuotes()
